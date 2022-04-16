31322bcd05917215e2893b80dab877beb1874878
AndreySchigolev <cobhc900@gmail.com>
2022-04-17 01:49:52 +0300

    Add Cart REST Controller

--- src/main/java/com/amr/project/dao/abstracts/CartItemReadWriteDao.java
+++ src/main/java/com/amr/project/dao/abstracts/CartItemReadWriteDao.java
@@ -1,6 +1,8 @@
 package com.amr.project.dao.abstracts;
 
 import com.amr.project.model.entity.CartItem;
+import java.util.List;
 
 public interface CartItemReadWriteDao extends ReadWriteDao<CartItem, Long> {
+    List<CartItem> findByUser (Long id);
 }
--- src/main/java/com/amr/project/service/abstracts/CartItemService.java
+++ src/main/java/com/amr/project/service/abstracts/CartItemService.java
@@ -1,6 +1,8 @@
 package com.amr.project.service.abstracts;
 
 import com.amr.project.model.entity.CartItem;
+import java.util.List;
 
 public interface CartItemService extends ReadWriteService<CartItem, Long> {
+    List<CartItem> findByUser(Long id);
 }
