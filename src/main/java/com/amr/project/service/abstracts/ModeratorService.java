ad9680abafcbd120289bf41d55475d9f8489ba7a
AndreySchigolev <cobhc900@gmail.com>
2022-04-22 14:35:02 +0300

    add security for moderator api

--- src/main/java/com/amr/project/service/abstracts/ModeratorService.java
+++ src/main/java/com/amr/project/service/abstracts/ModeratorService.java
@@ -2,13 +2,9 @@ package com.amr.project.service.abstracts;
 
 import com.amr.project.model.dto.ModeratorListDto;
 import com.amr.project.model.entity.Moderator;
-import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface ModeratorService {
-   @PreAuthorize("hasRole('ROLE_MODERATOR')")
    ModeratorListDto findAllUnModerated();
-   @PreAuthorize("hasRole('ROLE_MODERATOR')")
    void accept(Moderator moderator);
-   @PreAuthorize("hasRole('ROLE_MODERATOR')")
    void unAccept(Moderator moderator);
 }
