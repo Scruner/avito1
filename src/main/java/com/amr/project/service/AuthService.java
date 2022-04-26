8ceb5d3e136947e2b6413ecf84707149175a4f4b
Pryal1979 <90647814+Pryal1979@users.noreply.github.com>
2022-04-26 23:31:09 +0300

    2х факторная аутентификация

--- src/main/java/com/amr/project/service/AuthService.java
+++ src/main/java/com/amr/project/service/AuthService.java
@@ -1,19 +1,44 @@
 package com.amr.project.service;
 
-import com.amr.project.model.dto.AuthDto;
+import com.amr.project.model.dto.UserDto;
+import de.taimos.totp.TOTP;
+import org.apache.commons.codec.binary.Base32;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
 
-@Service
 public class AuthService {
- public AuthDto tempCode(String phoneNumber) {
-     // Метод принимает номер телефона, генерирует случайный пароль, возвращает его и
-     // и отправляет на номер пользователя
-     String passwordAuth= String.valueOf((long)(Math.random()*10000));
-     System.out.println("На номер: "+phoneNumber + " отправлен код : "  + passwordAuth);
-     LocalDateTime expired = LocalDateTime.now().plusSeconds(60);
-     AuthDto authDto = new AuthDto(passwordAuth,expired);
-     return authDto;
- }
+    public static String QR_PREFIX ="https://chart.googleapis.com/chart?chs=200x200&chld=M%%7C0&cht=qr&chl=";
+    public static String APP_NAME = "Avito";
+
+
+    public static String generateSecretKey() {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[20];
+        random.nextBytes(bytes);
+        Base32 base32 = new Base32();
+        return base32.encodeToString(bytes);
+    }
+
+    public static String getTOTPCode(String SecretKey) {
+        Base32 base32 = new Base32();
+        byte[] bytes = base32.decode(SecretKey);
+        String hexKey = Hex.encodeHexString(bytes);
+        return TOTP.getOTP(hexKey);
+    }
+
+    public static String getGoogleAuthenticatorBarCode(String secretKey, String account) {
+        try {
+            return QR_PREFIX+"otpauth://totp/"
+                    + URLEncoder.encode(APP_NAME + ":" + account, "UTF-8").replace("+", "%20")
+                    + "?secret=" + URLEncoder.encode(secretKey, "UTF-8").replace("+", "%20")
+                    + "&issuer=" + URLEncoder.encode(APP_NAME, "UTF-8").replace("+", "%20");
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
 }
--- src/main/java/com/amr/project/webapp/controller/TwoFactorAuthController.java
+++ src/main/java/com/amr/project/webapp/controller/TwoFactorAuthController.java
@@ -1,36 +1,34 @@
 package com.amr.project.webapp.controller;
 
-import com.amr.project.converter.UserMapper;
-import com.amr.project.model.dto.AuthDto;
 import com.amr.project.model.dto.UserDto;
 import com.amr.project.service.AuthService;
-import com.amr.project.service.abstracts.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDateTime;
-
 @RequestMapping("/user")
 public class TwoFactorAuthController {
-    private UserService userService;
-    private UserMapper userMapper;
-    private AuthService authService;
-    private AuthDto temp_authDto;
-
 
     @PostMapping(path="/factorAuth")
     public ResponseEntity<UserDto> factorAuth(@RequestBody UserDto userDto) {
         if (userDto.isUsingTwoFactorAuth()) {
-           temp_authDto = authService.tempCode(userDto.getPhone());
+            String s=AuthService.generateSecretKey();
+            userDto.setSecret(s);
+            userDto.setQr_code(AuthService.getGoogleAuthenticatorBarCode(s,userDto.getEmail()));
+            //необходимо вывести на фронте картинку QR кода и пользователь с помощью смартфона с установленным приложением
+            // Google Authenticator сканирует QR код и получает 6 значный номер и сохраняет его
+            // в поле activationCode.
         }
         return ResponseEntity.ok(userDto);
     }
+
+
     @PostMapping(path="/factorAuth/code")
-    public ResponseEntity<UserDto> validateCode(@RequestBody UserDto userDto, AuthDto authDto) {
-        if (temp_authDto.getCode().equals(authDto.getCode()) && (temp_authDto.getExpired().isBefore(LocalDateTime.now()))) {
+    public ResponseEntity<UserDto> validateCode(@RequestBody UserDto userDto) {
+        String expected_code= AuthService.getTOTPCode(userDto.getSecret());
+        if (expected_code.equals((userDto.getActivationCode()))) {
             return ResponseEntity.ok(userDto);
         }  else return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
